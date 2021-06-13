28aeb3003d409188c458636210a5cbde957e18ba
jamalkhlifat <84751413+jamalkhlifat@users.noreply.github.com>
2021-06-13 11:37:38 +0300

    Update uploadRouter.js

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -1,10 +1,6 @@
 import multer from 'multer';
 import express from 'express';
 import { isAuth } from '../utils.js';
-import multerS3 from 'multer-s3';
-import aws from 'aws-sdk';
-
-
 
 const uploadRouter = express.Router();
 
@@ -21,27 +17,6 @@ const upload = multer({ storage });
 
 uploadRouter.post('/', isAuth, upload.single('image'), (req, res) => {
   res.send(`/${req.file.path}`);
-});
-
-aws.config.update({
-  
-
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
-});
-const s3 = new aws.S3();
-const storageS3 = multerS3({
-  s3,
-  bucket: 'onclickjamal',
-  acl: 'public-read',
-  contentType: multerS3.AUTO_CONTENT_TYPE,
-  key(req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-const uploadS3 = multer({ storage: storageS3 });
-uploadRouter.post('/s3', uploadS3.single('image'), (req, res) => {
-  res.send(req.file.location);
 });
 
 export default uploadRouter;
