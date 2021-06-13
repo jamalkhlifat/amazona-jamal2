5f359eb2a7d7831acda4478a05bf86e00b50bafc
jamalkhlifat <84751413+jamalkhlifat@users.noreply.github.com>
2021-06-13 11:24:17 +0300

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -7,5 +7,7 @@ export default {
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
     PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+    accessKeyId: process.env.ACCESS_KEY_ID || 'accessKeyId',
+    secretAccessKey: process.env.SECRET_ACCESS_KEY || 'secretAccessKey',
    
-}
+};
