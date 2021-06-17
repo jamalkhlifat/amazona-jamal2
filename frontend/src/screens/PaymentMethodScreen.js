2cedf82e8c0040ac0d34436bea0fab5e437f0e23
jamalkhlifat <84751413+jamalkhlifat@users.noreply.github.com>
2021-06-17 10:26:55 +0300

    Update PaymentMethodScreen.js

--- frontend/src/screens/PaymentMethodScreen.js
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -41,13 +41,13 @@ export default function PaymentMethodScreen(props) {
           <div>
             <input
               type="radio"
-              id="stripe"
-              value="Stripe"
+              id="Cash"
+              value="Cash on Delivery"
               name="paymentMethod"
               required
               onChange={(e) => setPaymentMethod(e.target.value)}
             ></input>
-            <label htmlFor="stripe">Stripe</label>
+            <label htmlFor="Cash">Cash</label>
           </div>
         </div>
         <div>
